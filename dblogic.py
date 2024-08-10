ce87b7f410735286221850cddd5eaef55ec58476
Alexey Khristolyubov <hristolyubov@flagsoft.ru>
2024-08-11 00:40:01 +0500

    Fixing issues and adding docker-compose. I don't have an idea of why this thing had even worked at the first place

--- dblogic.py
+++ dblogic.py
@@ -6,21 +6,15 @@ import Girlclass
 import os
 
 dotenv.load()
+dbhost = os.getenv("DBHOST")
 dbuser = os.getenv("DBUSER")
 dbpass = os.getenv('DBPASS')
-name = "girls.db" #name of the database
-
+name = os.getenv("DBNAME")
 #this code makes a connection to database to see if it runs ok. Returns a tuple with all user data
-with psycopg2.connect(database=name, user=dbuser, password=dbpass) as con:
-    cursor = con.cursor()
-    cursor.execute('SELECT * FROM girls')
-    data = cursor.fetchall()
-    print(data)
-    print("Database is ready to go")
 
 #creates a record after registration.
 def makenewgirl(userid, takepill, mustpill, numinc):
-    with psycopg2.connect(database=name, user=dbuser, password=dbpass) as con:
+    with psycopg2.connect(database=name, user=dbuser, password=dbpass, host=dbhost) as con:
         cursor = con.cursor()
         cursor.execute(f"INSERT INTO girls (userid, takepill, mustpill, numinc) VALUES (%s, %s, %s, %s)", (userid, takepill, mustpill, numinc))
         con.commit()
@@ -28,7 +22,7 @@ def makenewgirl(userid, takepill, mustpill, numinc):
 
 #fetches all users and creates a list of tuples
 def fetchallusers():
-    with psycopg2.connect(database=name, user=dbuser, password=dbpass) as con:
+    with psycopg2.connect(database=name, user=dbuser, password=dbpass, host=dbhost) as con:
         cursor = con.cursor()
         cursor.execute('SELECT * FROM girls')
         userlist = cursor.fetchall()
@@ -36,7 +30,7 @@ def fetchallusers():
 
 #takes a string with userid and returns a tuple if finds one
 def getuser(userid):
-    with psycopg2.connect(database=name, user=dbuser, password=dbpass) as con:
+    with psycopg2.connect(database=name, user=dbuser, password=dbpass, host=dbhost) as con:
         cursor = con.cursor()
         cursor.execute("SELECT * FROM girls WHERE userid = %s", (userid, ))
         user = cursor.fetchall()
@@ -45,7 +39,7 @@ def getuser(userid):
 
 #takes in a string with userid and deletes a user if finds one
 def deleteuser(userid):
-    with psycopg2.connect(database=name, user=dbuser, password=dbpass) as con:
+    with psycopg2.connect(database=name, user=dbuser, password=dbpass, host=dbhost) as con:
         cursor = con.cursor()
         cursor.execute("DELETE FROM girls WHERE userid = %s", (userid,))
         con.commit()
@@ -58,14 +52,14 @@ def updateuser(object: Girlclass.Girl):
     takepill = object.get_takepill()
     mustpill = object.get_mustpill()
     numinc = object.get_numinc()
-    with sqlite3.connect(name) as con:
+    with psycopg2.connect(database=name, user=dbuser, password=dbpass, host=dbhost) as con:
         cursor = con.cursor()
         cursor.execute('UPDATE girls SET takepill = %s, mustpill = %s, numinc = %s WHERE userid = %s', (takepill, mustpill, numinc, userid))
         con.commit()
         print("Update complete fo -r" + object.get_userid())
 #Creates a database and a table
-if __name__ == '__main__':
-    with psycopg2.connect(database=name, user=dbuser, password=dbpass) as con:
+if __name__ != '__main__':
+    with psycopg2.connect(database=name, user=dbuser, password=dbpass, host=dbhost) as con:
         cursor = con.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS girls (
         userid INTEGER PRIMARY KEY,
--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.11
-COPY requirements.txt /dockerimage
+COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY main.py .
 COPY Girlclass.py .
